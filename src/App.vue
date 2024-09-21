da9ca70a520361b5b4c76634c70d23713b8b946d
LEFEVRE <lefevrejulien@live.fr>
2024-09-21 16:59:10 +0200

    vapour

--- src/App.vue
+++ src/App.vue
@@ -31,6 +31,41 @@
 
       </li>
     </ul>
+
+    <div :class="$style.container">
+
+      <div :class="$style.cup">
+
+        <div :class="$style.top">
+          <div :class="$style.vapour">
+            <span style="--i:1;"></span>
+            <span style="--i:3;"></span>
+            <span style="--i:16;"></span>
+            <span style="--i:5;"></span>
+            <span style="--i:13;"></span>
+            <span style="--i:20;"></span>
+            <span style="--i:6;"></span>
+            <span style="--i:7;"></span>
+            <span style="--i:10;"></span>
+            <span style="--i:8;"></span>
+            <span style="--i:17;"></span>
+            <span style="--i:11;"></span>
+            <span style="--i:12;"></span>
+            <span style="--i:14;"></span>
+            <span style="--i:2;"></span>
+            <span style="--i:9;"></span>
+            <span style="--i:15;"></span>
+            <span style="--i:4;"></span>
+            <span style="--i:19;"></span>
+            
+            
+          </div>
+          <div :class="$style.circle">
+            <div :class="$style.tea"></div>
+          </div>
+        </div>
+      </div>
+    </div>
     
   </div>
 </template>
@@ -82,6 +117,107 @@ return sortedTodos
 </script>
 
 <style module>
+
+body {
+  
+  
+  min-height: 100vh;
+  background: #607d8b;
+}
+
+.container {
+  position: relative;
+  top: 50px;
+}
+.cup
+{
+  position: relative;
+  width: 280px;
+  height: 300px;
+  background: linear-gradient(to right, #f9f9f9, #d9d9d9);
+  border-bottom-left-radius: 45%;
+  border-bottom-right-radius: 45%;
+}
+
+.top {
+  position: absolute;
+  top: -30px;
+  left: 0;
+  width: 100%;
+  height: 60px;
+  border-radius: 50%;
+  background: linear-gradient(to right, #f9f9f9, #d9d9d9);
+}
+
+.circle{
+  position: absolute;
+  top: 5px;
+  left: 10px;
+  width: calc(100% - 20px);
+  height: 50px;
+  border-radius: 50%;
+  box-sizing: border-box;
+  background: linear-gradient(to left, #f9f9f9, #d9d9d9);
+  overflow: hidden;
+}
+
+.tea{
+  position: absolute;
+  top: 20px;
+  left:0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(#5fd15f, #008000);
+  border-radius: 50%;
+
+
+}
+
+.vapour {
+  position: relative;
+  display: flex;
+  z-index: 1;
+  padding: 0 20px;
+}
+
+.vapour span {
+  position: relative;
+  bottom:50px;
+  display: block;
+  margin: 0 2px 50px;
+  min-width: 8px;
+  height: 120px;
+  background: #fff;
+  border-radius: 50%;
+  animation: animate 5s linear infinite;
+  filter: blur(8px);
+  animation-delay: calc(var(--i) * -0.5s);
+}
+
+@keyframes animate {
+  0%
+  {
+    transform: translateY(0) scaleX(1);
+    opacity: 0
+
+  }
+  15%
+  {
+    opacity: 1;
+  }
+  50%
+  {
+    transform: translateY(-150px) scaleX(5)
+  }
+  95%
+  {
+    opacity: 0;
+  }
+  100%
+  {
+    transform: translateY(-30px0) scaleX(10)
+  }
+}
 
 .completed {
   
